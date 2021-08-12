9e0bfac683be2ff4a3acd808c8335b98e651ccb1
hypnopump <ericalcaide1@gmail.com>
2021-08-12 15:54:54 +0200

    add autoregressive prediction

--- rgn2_replica/rgn2.py
+++ rgn2_replica/rgn2.py
@@ -13,16 +13,11 @@ class AminoBERT(torch.nn.Module):
         return
 
 
-class RGN2(torch.nn.Module): 
-    """ RGN2 module which turns embeddings into a Cα trace. """
-    def __init__(self, **kwargs): 
-        return
-
-
 class RGN2_LSTM_Naive(torch.nn.Module): 
     def __init__(self, layers, emb_dim=1280, hidden=[256, 128, 64], 
                  bidirectional=True, mlp_hidden=[32, 4]): 
-        """ Inputs: 
+        """ RGN2 module which turns embeddings into a Cα trace.
+            Inputs: 
             * layers: int. number of rnn layers
             * emb_dim: int. number of dimensions in the input
             * hidden: int or list of ints. hidden dim at each layer
@@ -51,10 +46,34 @@ class RGN2_LSTM_Naive(torch.nn.Module):
         )
     
     def forward(self, x): 
-        """ Inputs: (..., Emb_dim) -> Outputs: (..., 4). """
+        """ Inputs: (..., Emb_dim) -> Outputs: (..., 4). 
+			
+			Note: 4 last dims of input is angles of previous point.
+			      for first point, add dumb token [-5, -5, -5, -5]
+        """
         for i,rnn_layer in enumerate(self.stacked_lstm): 
             x, (h_n, c_n) = rnn_layer(x)
         return self.last_mlp(x)
+
+    def predict_fold(self, x):
+    	""" Autoregressively generates the protein fold
+    		Inputs: (L, Emb_dim) -> Outputs: (L, 4). 
+
+    		Note: 4 last dims of input is dumb token for first res. 
+    			  Use same as in `.forward()` method.
+    	"""
+    	preds_list = []
+    	for i in range(x.shape[-2]):
+    		if i == 0: 
+    			input_step = x[i:i+1, :]
+    		else: 
+    			# add angle predicton from pred step to this one.
+    			input_step = torch.cat([x[i:i+1, :-4], preds_list[-1][:, -4:]])
+
+    		preds_list.append( self.forward(input_step) )
+
+    	return torch.cat(preds_list, dim=-2) 
+    	
 
 
 class Refiner(torch.nn.Module): 
