9d69683af32fa4799e9b4f2f390eda2a10b74b37
Habib Bukhari <bukharih@dingx-ws1.int.janelia.org>
2021-08-16 18:30:12 -0400

    added pfam, go loss and loss_wrapper thats combines AminoBret Loss and pfamgoLoss

--- rgn2_replica/losses.py
+++ rgn2_replica/losses.py
@@ -1,8 +1,10 @@
 import torch
 from torch import nn
+from typing import Optional
+import torch.nn.functional as F
+
 
 class AminoBERTLoss(nn.Module):
-
     def __init__(self, padding_token=-100, vocab_size=24):
         super().__init__()
         self.masked_loss = nn.CrossEntropyLoss(
@@ -12,11 +14,7 @@ class AminoBERTLoss(nn.Module):
         self.vocab_size = vocab_size
 
     def forward(
-        self,
-        logit_out: torch.Tensor,
-        logit_chunk_perm: torch.Tensor,
-        target: torch.Tensor,
-        chunk_perm: torch.Tensor,
+        self, logit_out=None, logit_chunk_perm=None, target=None, chunk_perm=None,
     ):
         """
         logit_out:  (bs, len, vocab_size) tensor
@@ -24,6 +22,8 @@ class AminoBERTLoss(nn.Module):
         target: (bs, len) tensor
         chunk_perm: (bs, 1)
         """
+
+        # to do: Check Logic
         global_petrub = 1 - chunk_perm
 
         masked_lm_loss = (
@@ -36,4 +36,58 @@ class AminoBERTLoss(nn.Module):
         loss = (chunk_perm * chunk_perm_loss) + (1 - global_petrub * masked_lm_loss)
 
         return loss.mean()
-    
+
+
+class GoPFAMLoss(nn.Module):
+    def __init__(self, weights=[1., 1.]):
+        """
+        weights: [float, float] weights for combining loss for go and pfam
+        """
+        super().__init__()
+        self.weights = weights
+
+    def forward(self, logit_go=None, logit_pfam=None, target_go=None, target_pfam=None):
+
+        go_loss = F.cross_entropy(logit_go, target_go)
+        pfam_loss = F.cross_entropy(logit_pfam, target_pfam)
+        combined_loss = go_loss * self.weights[0] + pfam_loss * self.weights[0]
+        return combined_loss
+
+
+class LossWrapper(nn.Module):
+    def __init__(self, lm_loss: None, aux_loss: None, weights=[1., 1.]):
+        """
+        Combines AminoBERTLoss with GoPFAMLoss
+        """
+        super().__init__()
+        self.lm_loss = lm_loss
+        self.aux_loss = aux_loss
+        self.weights = weights
+
+    def forward(
+        self,
+        logit_out=None,
+        logit_chunk_perm=None,
+        logit_go=None,
+        logit_pfam=None,
+        target_go=None,
+        target_pfam=None,
+        chunk_perm=None,
+        target=None,
+    ):
+
+        lm_loss = self.lm_loss(
+            logit_out=logit_out,
+            logit_chunk_perm=logit_chunk_perm,
+            target=target,
+            chunk_perm=chunk_perm,
+        )
+        aux_loss = self.aux_loss(
+            logit_go=logit_go,
+            logit_pfam=logit_pfam,
+            target_go=target_go,
+            target_pfam=target_pfam,
+        )
+        combined_loss = lm_loss * self.weights[0] + aux_loss * self.weights[1]
+
+        return combined_loss
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -14,3 +14,53 @@ def test_aminobert_loss():
     loss = loss_func(logit_out, logit_chunk_perm, target, chunk_perm)
     assert True
 
+
+def test_go_pfam_loss():
+    num_classes = 45
+    bs = 40
+    target_pfam = torch.randint(0, num_classes, (bs,))
+    target_go = torch.randint(0, num_classes, (bs,))
+    logit_pfam = torch.rand(bs, num_classes)
+    logit_go = torch.rand(bs, num_classes)
+    weights = [2, 3]
+    loss_func = GoPFAMLoss(weights=weights)
+    loss = loss_func(
+        logit_go=logit_go,
+        logit_pfam=logit_pfam,
+        target_go=target_go,
+        target_pfam=target_pfam,
+    )
+    
+    assert True
+    
+def test_loss_wrapper():
+    num_classes = 45
+    bs = 40
+    target_pfam = torch.randint(0, num_classes, (bs,))
+    target_go = torch.randint(0, num_classes, (bs,))
+    logit_pfam = torch.rand(bs, num_classes)
+    logit_go = torch.rand(bs, num_classes)
+    weights = [2, 0.6]
+    loss_go_pfam = GoPFAMLoss(weights=weights)
+
+
+    vocab_size = 24
+    logit_out = torch.rand(bs, 10, vocab_size)
+    logit_chunk_perm = torch.rand(bs, 2)
+    target = torch.randint(1, 20, (bs, 10))
+    chunk_perm = torch.randint(0, 2, (bs,))
+    loss_lm = AminoBERTLoss(vocab_size=vocab_size)
+
+
+    combine_loss = LossWrapper(lm_loss=loss_lm, aux_loss=loss_go_pfam, weights=[2, 3])
+    loss = combine_loss(
+        logit_out=logit_out,
+        logit_chunk_perm=logit_chunk_perm,
+        logit_go=logit_go,
+        logit_pfam=logit_pfam,
+        target_go=target_go,
+        target_pfam=target_pfam,
+        chunk_perm=chunk_perm,
+        target=target,
+    )
+    assert True
