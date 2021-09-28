555afd11eed067a2bc3529f48048fc71dddbbcd8
ericalcaide <ericalcaide1@gmail.com>
2021-09-28 14:02:13 +0200

    fix diff

--- rgn2_replica/rgn2_trainers.py
+++ rgn2_replica/rgn2_trainers.py
@@ -455,11 +455,10 @@ def train(get_prot_, steps, model, embedder, optim, loss_f=None,
             )
 
             # violation loss btween calphas - L1
-            dist_mat = mp_nerf.utils.cdist(infer["wrapper_pred"][:, :, 1], 
-                                           infer["wrapper_pred"][:, :, 1],) # B, L, L
-            dist_mat[:, np.arange(dist_mat.shape[-1]), np.arange(dist_mat.shape[-1])] = \
-                dist_mat[:, np.arange(dist_mat.shape[-1]), np.arange(dist_mat.shape[-1])] + 5.
-            viol_loss = -(dist_mat - 3.78).clamp(min=-np.inf, max=0.) 
+            dist_mat = torch.cdist(infer["wrapper_pred"][:, :, 1], 
+                                   infer["wrapper_pred"][:, :, 1],) # B, L, L
+            dist_mat[:, np.arange(dist_mat.shape[-1]), np.arange(dist_mat.shape[-1])] = 5.
+            viol_loss = -(dist_mat - 3.78).clamp(min=-np.inf, max=0.).contiguous()
             
             # calc metrics
             log_dict = {
